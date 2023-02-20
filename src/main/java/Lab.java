3ef804b4c6a000b95c6dac7bfd66143225a0c845
ChristianPritchard <christian.pritchard14@gmail.com>
2023-02-20 17:38:41 +0000

    Instantiation lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,9 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+        Object orange = new Object();
+
+        return orange;
     }
 }
